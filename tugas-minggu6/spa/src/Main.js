174d5849d58c208cf0a28da875f4c1e2c888edf7
aflahbrilli <aflahbrilli891@gmail.com>
2020-03-18 13:14:48 +0700

    mengerjakan pratikum minggu 7

---
+++ tugas-minggu6/spa/src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <div>
+                <ul className="header">
+                <li><a href="/">Home</a></li>
+                <li><a href="offer">Our Offer</a></li>
+                <li><a href="find">Find Us</a></li>
+                </ul>
+                <div className="content">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Main;
