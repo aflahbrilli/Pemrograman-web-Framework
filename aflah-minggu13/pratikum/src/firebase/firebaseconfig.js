082d4feed84fcbb50d148b3656646ca815df862a
aflahbrilli <aflahbrilli891@gmail.com>
2020-05-01 19:44:39 +0700

    Praktikum minggu 13

---
+++ aflah-minggu13/pratikum/src/firebase/firebaseconfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAihNmQN4gxho_bLlUmt7t53vIWSF4sUzA",
+    authDomain: "aflah-minggu13.firebaseapp.com",
+    databaseURL: "https://aflah-minggu13.firebaseio.com",
+    projectId: "aflah-minggu13",
+    storageBucket: "aflah-minggu13.appspot.com",
+    messagingSenderId: "844175870888",
+    appId: "1:844175870888:web:66280beee136d08b4bab9d",
+    measurementId: "G-Y6WB8SMGBB"
+  };
+
+  export default firebaseConfig;
